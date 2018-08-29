d8e9f07552be3532f06153c21a613a9d45d14c0c
Wayne Teutenberg <Teutenberg@users.noreply.github.com>
2018-08-29 14:21:44 +1200

    update

--- Automation/logshipping_config.ps1
+++ Automation/logshipping_config.ps1
@@ -1,33 +1,54 @@
-$InstName = "MSSQLSERVER"
-$SqlAdminGroup = "DOMAIN\MSSQL-Admins"
-$LsDirName = "logshipping"
+$PrimaryServerInstance   = 'DEMO\TEST6' 
+$PrimaryLogshippingDir   = 'G:\logship_send'
 
-$RegPath = "HKLM:\SOFTWARE\Microsoft\Microsoft SQL Server\" + $(Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Microsoft SQL Server\Instance Names\SQL').$InstName;
-$BakPath = $(Get-ItemProperty "$RegPath\\MSSQLServer").BackupDirectory
-$LsPath = Join-Path -Path $BakPath -ChildPath $LsDirName
-$NtServiceSql = "NT Service\MSSQL"
-$NtServiceAgt = "NT Service\SQL"
+$SecondaryServerInstance = 'DEMO\TEST5'
+$SecondaryLogshippingDir = 'G:\logship_receive'
 
-if (($InstName.Length -eq 0) -or ($InstName -eq "MSSQLSERVER"))
-{
-    $NtServiceSql = $NtServiceSql + "SERVER"
-    $NtServiceAgt = $NtServiceAgt + "SERVERAGENT"
+$SqlSysAdminGroup = 'administrators'
+
+$pComputerName = $PrimaryServerInstance.Split('\')[0]
+$pInstanceName = $PrimaryServerInstance.Split('\')[1]
+$pShareName = Split-Path $PrimaryLogshippingDir -Leaf
+$pServiceNameSql = if ($pInstanceName -and $pInstanceName -ine 'MSSQLSERVER') { 'MSSQL$' + $pInstanceName } else { 'MSSQLSERVER' } 
+$pServiceNameAgt = if ($pInstanceName -and $pInstanceName -ine 'MSSQLSERVER') { 'SQLAGENT$' + $pInstanceName } else { 'SQLSERVERAGENT' } 
+$pServiceAccountSql = (Get-WmiObject Win32_Service -ComputerName $pComputerName | Where-Object { $_.Name -like $pServiceNameSql }).StartName
+$pServiceAccountAgt = (Get-WmiObject Win32_Service -ComputerName $pComputerName | Where-Object { $_.Name -like $pServiceNameAgt }).StartName
+
+$sComputerName = $SecondaryServerInstance.Split('\')[0]
+$sInstanceName = $SecondaryServerInstance.Split('\')[1]
+$sShareName = Split-Path $SecondaryLogshippingDir -Leaf
+$sServiceNameSql = if ($sInstanceName -and $sInstanceName -ine 'MSSQLSERVER') { 'MSSQL$' + $sInstanceName } else { 'MSSQLSERVER' } 
+$sServiceNameAgt = if ($sInstanceName -and $sInstanceName -ine 'MSSQLSERVER') { 'SQLAGENT$' + $sInstanceName } else { 'SQLSERVERAGENT' } 
+$sServiceAccountSql = (Get-WmiObject Win32_Service -ComputerName $sComputerName | Where-Object { $_.Name -like $sServiceNameSql }).StartName
+$sServiceAccountAgt = (Get-WmiObject Win32_Service -ComputerName $sComputerName | Where-Object { $_.Name -like $sServiceNameAgt }).StartName
+
+Invoke-Command -ComputerName $pComputerName -ScriptBlock {if (!(Test-Path $Using:PrimaryLogshippingDir)) { New-Item $Using:PrimaryLogshippingDir -type directory }}
+Invoke-Command -ComputerName $sComputerName -ScriptBlock {if (!(Test-Path $Using:SecondaryLogshippingDir)) { New-Item $Using:SecondaryLogshippingDir -type directory }}
+
+Invoke-Command -ComputerName $pComputerName -ScriptBlock {
+    $acl = Get-Acl $Using:PrimaryLogshippingDir
+    $acl.SetAccessRule($(New-Object Security.AccessControl.FileSystemAccessRule($Using:SqlSysAdminGroup,"FullControl","ContainerInherit,ObjectInherit","None","Allow")))
+    $acl.SetAccessRule($(New-Object system.security.accesscontrol.filesystemaccessrule($Using:pServiceNameSql,"FullControl","ContainerInherit,ObjectInherit","None","Allow")))
+    $acl.SetAccessRule($(New-Object system.security.accesscontrol.filesystemaccessrule($Using:sServiceNameAgt,"FullControl","ContainerInherit,ObjectInherit","None","Allow")))
+    Set-Acl $Using:PrimaryLogshippingDir $acl
+
+    if (Get-SmbShare | Where { $_.Name -eq $Using:pShareName }) {
+        Grant-SmbShareAccess -Name $Using:pShareName -AccountName $Using:SqlSysAdminGroup, $Using:pServiceNameSql, $Using:pServiceNameAgt, $Using:sServiceNameSql, $Using:sServiceNameAgt -AccessRight Full -Force
+    } else {
+        New-SmbShare –Name $Using:pShareName –Path $Using:PrimaryLogshippingDir -FullAccess $Using:SqlSysAdminGroup, $Using:pServiceNameSql, $Using:pServiceNameAgt, $Using:sServiceNameSql, $Using:sServiceNameAgt
+    }
 }
-else
-{
-    $NtServiceSql = $NtServiceSql + "`$$InstName"
-    $NtServiceAgt = $NtServiceAgt + "AGENT`$$InstName"
+
+Invoke-Command -ComputerName $sComputerName -ScriptBlock {
+    $acl = Get-Acl $Using:SecondaryLogshippingDir
+    $acl.SetAccessRule($(New-Object Security.AccessControl.FileSystemAccessRule($Using:SqlSysAdminGroup,"FullControl","ContainerInherit,ObjectInherit","None","Allow")))
+    $acl.SetAccessRule($(New-Object system.security.accesscontrol.filesystemaccessrule($Using:sServiceNameSql,"FullControl","ContainerInherit,ObjectInherit","None","Allow")))
+    $acl.SetAccessRule($(New-Object system.security.accesscontrol.filesystemaccessrule($Using:sServiceNameAgt,"FullControl","ContainerInherit,ObjectInherit","None","Allow")))
+    Set-Acl $Using:SecondaryLogshippingDir $acl
+
+    if (Get-SmbShare | Where { $_.Name -eq $Using:sShareName }) {
+        Grant-SmbShareAccess -Name $Using:sShareName -AccountName $Using:SqlSysAdminGroup, $Using:sServiceNameSql, $Using:sServiceNameAgt -AccessRight Full -Force
+    } else {
+        New-SmbShare –Name $Using:sShareName –Path $Using:SecondaryLogshippingDir -FullAccess $Using:SqlSysAdminGroup, $Using:sServiceNameSql, $Using:sServiceNameAgt
+    }
 }
-
-If (!(Test-Path $LsPath))
-{
-    New-Item $LsPath -type directory
-}
-
-$Acl = Get-Acl $LsPath
-$Acl.SetAccessRule($(New-Object Security.AccessControl.FileSystemAccessRule($SqlAdminGroup,"FullControl","ContainerInherit,ObjectInherit","None","Allow")))
-$Acl.SetAccessRule($(New-Object system.security.accesscontrol.filesystemaccessrule($NtServiceSql,"FullControl","ContainerInherit,ObjectInherit","None","Allow")))
-$Acl.SetAccessRule($(New-Object system.security.accesscontrol.filesystemaccessrule($NtServiceAgt,"FullControl","ContainerInherit,ObjectInherit","None","Allow")))
-Set-Acl $LsPath $Acl
-
-New-SmbShare –Name $LsDirName –Path $LsPath -FullAccess $SqlAdminGroup, $NtServiceSql, $NtServiceAgt
